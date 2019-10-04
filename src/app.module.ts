41902bb69794663aeabd2c219144a40ad1bcadc9
UtopiaBeam <utopiabeam@gmail.com>
2019-10-04 11:59:36 +0700

    :card_file_box: :sparkles: Add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
     imports: [
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (config: ConfigService) => ({
+                uri: config.mongoUrl,
+                useNewUrlParser: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true,
+            }),
+        }),
         ConfigModule,
     ],
     controllers: [AppController],
