e2f81cb8ac18ede5f6580c6434f04765caff4391
UtopiaBeam <utopiabeam@gmail.com>
2019-10-19 10:11:27 +0700

    :bug: :sparkles: Inject MongooseModule to UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModel, UserSchema } from './user.schema';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{ name: UserModel, schema: UserSchema }]),
+    ],
     controllers: [],
     providers: [UserService],
 })
